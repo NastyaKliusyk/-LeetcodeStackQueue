b5ee44460847bfa1f8603e4bd9b73bdeb8717a7c
NastyaKliusyk <kliusyk.pn@ucu.edu.ua>
2025-04-14 19:42:48 +0300

    Update Maximum Frequency Stack.py
    Changes using dequeue from collections

--- Maximum Frequency Stack /Maximum Frequency Stack.py
+++ Maximum Frequency Stack /Maximum Frequency Stack.py
@@ -1,23 +1,23 @@
+from collections import defaultdict, deque
+
 class FreqStack:
 
     def __init__(self):
-        self.freq_map = {}
-        self.group_map = {}
+        self.freq_map = defaultdict(int)
+        self.group_map = defaultdict(deque)
         self.max_freq = 0
 
     def push(self, val: int) -> None:
-        freq = self.freq_map.get(val, 0) + 1
+        freq = self.freq_map[val] + 1
         self.freq_map[val] = freq
 
         self.max_freq = max(self.max_freq, freq)
 
-        if freq not in self.group_map:
-            self.group_map[freq] = []
         self.group_map[freq].append(val)
 
     def pop(self) -> int:
-
         most_frequent = self.group_map[self.max_freq].pop()
+
         self.freq_map[most_frequent] -= 1
 
         if not self.group_map[self.max_freq]:
